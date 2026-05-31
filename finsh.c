3469055f25941a7baca39f257cc4c5caeb4b20b2
Chance Crosson <chancecrosson@gmail.com>
2026-05-31 11:18:48 -0500

    added fork/execvp functionality to execute path binaries

--- finsh.c
+++ finsh.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main(void) {
   char *cmd = NULL;
+  char *argv[3];
   size_t buflen = 0;
   ssize_t nread;
 
@@ -15,7 +19,21 @@ int main(void) {
       break;
     }
 
-    printf("%s", cmd);
+    if (nread > 0 && cmd[nread - 1] == '\n') {
+      cmd[nread - 1] = '\0';
+    }
+
+    pid_t pid = fork();
+
+    if (pid == -1) {
+      perror("finsh: An error occurred.");
+      continue;
+    } else if (pid == 0) {
+      execvp(cmd, argv);
+    } else {
+      int status;
+      waitpid(pid, &status, 0);
+    }
   }
 
   free(cmd);
